d2b6a7efee73b0024fc2e75c85b42f943c230919
juan roberto rosales vargas <juanros13@gmail.com>
2020-12-22 23:54:45 -0600

    primer commit

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -10,12 +10,12 @@ export const useFetchGifs = ( category ) => {
     useEffect(() => {
         getGifs(category)
             .then(imgs => {
-                setTimeout(() => {
+                // setTimeout(() => {
                     setState({
                         data: imgs,
                         loading: false
                     });
-                }, 3000);
+                // }, 3000);
             });
     }, [category])
 
